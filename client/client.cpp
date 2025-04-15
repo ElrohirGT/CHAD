e0be5ce1d8084a212b9af34f04dffaf0075f15fc
Jose-Prince <joseprince148@gmail.com>
2025-04-14 20:33:29 -0600

    Feat: render list of users connected (burned list)

--- client/client.cpp
+++ client/client.cpp
@@ -10,7 +10,18 @@
 #include <QCloseEvent>
 #include <QPushButton>
 #include <QDebug>
+#include <QListView>
+#include <QStringListModel>
+#include <QPainter>
+#include <QVariant>
+#include <QObject>
 #include <cstdio>
+#include <cstring>
+#include <vector>
+#include <QStyledItemDelegate>
+#include <QModelIndex>
+#include <QAbstractItemView>
+
 
 // class for creating the window project
 class MainWindow : public QMainWindow {
@@ -32,11 +43,11 @@ class ActiveButton : public QPushButton {
         ActiveButton(bool &RefStatus, QWidget *parent = nullptr) : QPushButton(parent), status(false), externalStatus(RefStatus) {
             setMaximumWidth(50);
             setContentsMargins(0,0,0,100);
-
+            
             connect(this, &QPushButton::clicked, this, &ActiveButton::clickSlot);
         }
 
-    public slots:
+        public slots:
         // Change to opposite status set status true for Busy
         void clickSlot(){
             status = !status; // change class status
@@ -45,26 +56,126 @@ class ActiveButton : public QPushButton {
             qDebug() << "main status:" << externalStatus; // Debug print
         }
     
-    // Class attributes
-    private:
+        // Class attributes
+        private:
         bool status;
         bool &externalStatus;
 };
 
-int main(int argc, char *argv[]) {
+class UWUUserQT : public QWidget {
 
-    bool isBusy = false;
-    char name[] = "Jose"; 
+    public:
+        UWUUserQT(const char username[], const char ip[], QWidget *parent = nullptr) : QWidget(parent) {
+    
+        std::strncpy(username_, username, sizeof(username_) - 1);
+        username_[sizeof(username_) - 1] = '\0';
+    
+        std::strncpy(ip_, ip, sizeof(ip_) - 1);
+        ip_[sizeof(ip_) - 1] = '\0';
+        QVBoxLayout *verticalLayout = new QVBoxLayout(this);
+        QLabel *nameLabel = new QLabel(username);
+        QLabel *ipLabel = new QLabel(ip);
+        
+        verticalLayout->addWidget(nameLabel);
+        verticalLayout->addWidget(ipLabel);
+    
+    }
+    
+    
+    private:
+        char username_[255];    
+        char ip_[255];
+    
+}; 
+
+struct UserData {
+    QString name;
+    QString ip;
+};
+
+class UWUUserModel : public QAbstractListModel {
+public:
+    UWUUserModel(const std::vector<UserData>& users, QObject *parent = nullptr)
+        : QAbstractListModel(parent), userDataList(users) {}
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override {
+        return parent.isValid() ? 0 : userDataList.size();
+    }
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override {
+        if (!index.isValid() || index.row() < 0 || index.row() >= userDataList.size()) {
+            return QVariant();
+        }
+
+        const UserData& user = userDataList[index.row()];
+
+        if (role == Qt::DisplayRole) {
+            // Puedes retornar algo simple para el texto por defecto si es necesario
+            return user.name;
+        } else if (role == Qt::UserRole + 1) {
+            // Retornar los datos del usuario para el delegado
+            return QVariant::fromValue(user);
+        }
+        return QVariant();
+    }
+
+private:
+    std::vector<UserData> userDataList;
+};
+
+class UWUUserDelegate : public QStyledItemDelegate {
+    public:
+        UWUUserDelegate(QObject *parent = nullptr) : QStyledItemDelegate(parent) {}
+    
+        void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override {
+            if (index.isValid()) {
+                QVariant userDataVar = index.data(Qt::UserRole + 1);
+                if (userDataVar.canConvert<UserData>()) {
+                    UserData userData = userDataVar.value<UserData>();
+                    UWUUserQT userWidget(userData.name.toLatin1(), userData.ip.toLatin1());
+    
+                    painter->save();
+                    painter->translate(option.rect.topLeft());
+                    userWidget.render(painter);
+                    painter->restore();
+                } else {
+                    QStyledItemDelegate::paint(painter, option, index); // Fallback
+                }
+            }
+        }
+    
+        QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override {
+            UWUUserQT tempWidget("dummy", "dummy");
+            return tempWidget.sizeHint();
+        }
+};
+
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
+    std::vector<UserData> users = {
+        {"Jose", "1.2.3"},
+        {"Maria", "4.5.6"},
+        {"Pedro", "7.8.9"}
+    };
+    
+    
+    bool isBusy = false;
+    char name[] = "Jose"; 
+    
     // Crear la ventana principal
     MainWindow mainWindow;
+    
+    UWUUserModel *userModel = new UWUUserModel(users);
+    QListView *chatUsers = new QListView();
+    chatUsers->setModel(userModel);
 
-    QListWidget *chatUsers = new QListWidget(&mainWindow);
-
+    UWUUserDelegate *userDelegate = new UWUUserDelegate();
+    chatUsers->setItemDelegate(userDelegate);
+    
     // Create button for handling busy status
     ActiveButton *activeButton = new ActiveButton(isBusy);
-
+    
     // Generates Widgets
     // QWidgets are the base component for the QT aplicattion
     // Contains all widgets
@@ -102,8 +213,6 @@ int main(int argc, char *argv[]) {
     nameLabel->setContentsMargins(10,0,0,0);
     nameLabel->setStyleSheet("font-size: 25px;");
 
-    QLabel *chatListLabel = new QLabel("Lista de Usuarios");
-    chatListLabel->setMinimumWidth(300);
     QLabel *chatAreaLabel = new QLabel("Área de Chat");
     
     //Button for changing status to Busy or Active
@@ -112,17 +221,12 @@ int main(int argc, char *argv[]) {
     statusButton->setContentsMargins(0, 0, 0, 100);
 
     QPalette topPalette = nameLabel->palette();
-    topPalette.setColor(QPalette::Window, QColor(31, 181, 25)); // Color azul claro
+    topPalette.setColor(QPalette::Window, QColor(31, 181, 25));
     topWidget->setAutoFillBackground(true);
     topWidget->setPalette(topPalette);
 
-    QPalette chatListPalette = chatListLabel->palette();
-    chatListPalette.setColor(QPalette::Window, QColor(189, 189, 189)); // Color naranja claro
-    chatListWidget->setAutoFillBackground(true);
-    chatListWidget->setPalette(chatListPalette);
-
     QPalette chatAreaPalette = chatAreaLabel->palette();
-    chatAreaPalette.setColor(QPalette::Window, QColor(189, 189, 189)); // Color verde claro
+    chatAreaPalette.setColor(QPalette::Window, QColor(189, 189, 189));
     chatWidget->setAutoFillBackground(true);
     chatWidget->setPalette(chatAreaPalette);
 
@@ -134,8 +238,8 @@ int main(int argc, char *argv[]) {
     topLayout->addWidget(activeButton);
     topWidget->setLayout(topLayout);
     
-    chatListLayout->addWidget(chatListLabel);
     chatListWidget->setLayout(chatListLayout);
+    chatListLayout->addWidget(chatUsers);
     
     chatAreaLayout->addWidget(chatAreaLabel);
     chatWidget->setLayout(chatAreaLayout);
