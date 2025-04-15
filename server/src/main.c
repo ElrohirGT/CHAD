b6370fffe2551af56c9e6734176234db39c3f655
ElrohirGT <elrohirgt@gmail.com>
2025-04-15 12:25:10 -0600

    feat: Added termination handling

--- server/src/main.c
+++ server/src/main.c
@@ -6,6 +6,8 @@
 #include "../../lib/lib.c"
 #include "../deps/hashmap/hashmap.h"
 #include "../deps/mongoose/mongoose.c"
+#include <signal.h>
+#include <stdio.h>
 
 /* *****************************************************************************
 Constants
@@ -17,17 +19,18 @@ static const char *s_cert_path = "cert.pem";
 static const char *s_key_path = "key.pem";
 
 // Global group chat
-static const UWU_String GROUP_CHAT_CHANNEL = {.data = "~", .length = 1};
+// static const UWU_String GROUP_CHAT_CHANNEL = {.data = "~", .length = 1};
 
 // The max quantity of messages a chat history can hold...
 // This value CAN'T be higher than 255 since that's the maximum number of
 // messages that can be sent over the wire.
-static const size_t MAX_MESSAGES_PER_CHAT = 100;
+// static const size_t MAX_MESSAGES_PER_CHAT = 100;
 
 // The amount of seconds that need to pass in order for a user to become IDLE.
-static const time_t IDLE_SECONDS_LIMIT = 15;
+// static const time_t IDLE_SECONDS_LIMIT = 15;
 // The amount of seconds that we wait before checking for IDLE users again.
-static const struct timespec IDLE_CHECK_FREQUENCY = {.tv_sec = 3, .tv_nsec = 0};
+// static const struct timespec IDLE_CHECK_FREQUENCY = {.tv_sec = 3, .tv_nsec =
+// 0};
 
 /* *****************************************************************************
 Server State
@@ -57,6 +60,8 @@ static UWU_ServerState *UWU_STATE = NULL;
 
 UWU_ServerState initialize_server_state(UWU_Err err) {
   UWU_ServerState state = {};
+
+  mg_mgr_init(&state.manager);
 
   state.active_usernames = UWU_UserList_init(err);
   if (err != NO_ERROR) {
@@ -99,13 +104,19 @@ UWU_ServerState initialize_server_state(UWU_Err err) {
 void deinitialize_server_state(UWU_ServerState *state) {
   state->is_shutting_off = TRUE;
 
-  fprintf(stderr, "Cleaning User List...\n");
+  fprintf(stderr, "Info: Deinitializing mongoose manager...\n");
+  mg_mgr_free(&state->manager);
+
+  fprintf(stderr, "Info: Cleaning User List...\n");
   UWU_UserList_deinit(&state->active_usernames);
-  fprintf(stderr, "Cleaning group Chat history...\n");
+
+  fprintf(stderr, "Info: Cleaning group Chat history...\n");
   UWU_ChatHistory_deinit(&state->group_chat);
-  fprintf(stderr, "Cleaning DM Chat histories...\n");
+
+  fprintf(stderr, "Info: Cleaning DM Chat histories...\n");
   hashmap_destroy(&state->chats);
-  fprintf(stderr, "Cleaning request arena...\n");
+
+  fprintf(stderr, "Info: Cleaning request arena...\n");
   UWU_Arena_deinit(state->req_arena);
 }
 
@@ -115,7 +126,7 @@ Utilities functions
 void update_last_action(UWU_User *info) {
   info->last_action = time(NULL);
   if ((time_t)-1 == info->last_action) {
-    UWU_PANIC("Fatal: Failed to obtain curren time!");
+    UWU_PANIC("Fatal: Failed to obtain current time!");
     return;
   }
 }
@@ -158,53 +169,53 @@ UWU_String create_changed_status_message(UWU_Arena *arena, UWU_User *info) {
 /* *****************************************************************************
 IDLE Detector
 ***************************************************************************** */
-static void *idle_detector(void *p) {
-  UWU_Err err = NO_ERROR;
-  UWU_Arena arena = UWU_Arena_init(2 + 1 + 255, err);
-  if (err != NO_ERROR) {
-    UWU_PANIC("Fatal: Failed to initialize idle_detector arena!");
-    return NULL;
-  }
-
-  // UWU_UserList *active_usernames = (UWU_UserList *)p;
-  // fprintf(stderr, "Info: active_usernames received in: %p\n",
-  //         (void *)&UWU_STATE->active_usernames);
-  UWU_UserList usernames = UWU_STATE->active_usernames;
-  while (!UWU_STATE->is_shutting_off) {
-    fprintf(stderr, "Info: Checking to IDLE %zu active users...\n",
-            usernames.length);
-    time_t now = time(NULL);
-
-    if ((clock_t)-1 == now) {
-      UWU_PANIC("Fatal: Failed to get current clock time!");
-      UWU_Arena_deinit(arena);
-      return NULL;
-    }
-
-    for (struct UWU_UserListNode *current = usernames.start; current != NULL;
-         current = current->next) {
-      if (current->is_sentinel) {
-        continue;
-      }
-
-      time_t seconds_diff = difftime(now, current->data.last_action);
-      UWU_ConnStatus status = current->data.status;
-      if (seconds_diff >= IDLE_SECONDS_LIMIT && status != INACTIVE) {
-        UWU_Arena_reset(&arena);
-        fprintf(stderr, "Info: Updating %.*s as INACTIVE!\n",
-                current->data.username.length, current->data.username.data);
-        current->data.status = INACTIVE;
-        UWU_String msg = create_changed_status_message(&arena, &current->data);
-        broadcast_msg(&UWU_STATE->manager, &msg);
-      }
-    }
-
-    nanosleep(&IDLE_CHECK_FREQUENCY, NULL);
-  }
-
-  UWU_Arena_deinit(arena);
-  return NULL;
-}
+// static void *idle_detector(void *p) {
+//   UWU_Err err = NO_ERROR;
+//   UWU_Arena arena = UWU_Arena_init(2 + 1 + 255, err);
+//   if (err != NO_ERROR) {
+//     UWU_PANIC("Fatal: Failed to initialize idle_detector arena!");
+//     return NULL;
+//   }
+//
+//   // UWU_UserList *active_usernames = (UWU_UserList *)p;
+//   // fprintf(stderr, "Info: active_usernames received in: %p\n",
+//   //         (void *)&UWU_STATE->active_usernames);
+//   UWU_UserList usernames = UWU_STATE->active_usernames;
+//   while (!UWU_STATE->is_shutting_off) {
+//     fprintf(stderr, "Info: Checking to IDLE %zu active users...\n",
+//             usernames.length);
+//     time_t now = time(NULL);
+//
+//     if ((clock_t)-1 == now) {
+//       UWU_PANIC("Fatal: Failed to get current clock time!");
+//       UWU_Arena_deinit(arena);
+//       return NULL;
+//     }
+//
+//     for (struct UWU_UserListNode *current = usernames.start; current != NULL;
+//          current = current->next) {
+//       if (current->is_sentinel) {
+//         continue;
+//       }
+//
+//       time_t seconds_diff = difftime(now, current->data.last_action);
+//       UWU_ConnStatus status = current->data.status;
+//       if (seconds_diff >= IDLE_SECONDS_LIMIT && status != INACTIVE) {
+//         UWU_Arena_reset(&arena);
+//         fprintf(stderr, "Info: Updating %.*s as INACTIVE!\n",
+//                 current->data.username.length, current->data.username.data);
+//         current->data.status = INACTIVE;
+//         UWU_String msg = create_changed_status_message(&arena,
+//         &current->data); broadcast_msg(&UWU_STATE->manager, &msg);
+//       }
+//     }
+//
+//     nanosleep(&IDLE_CHECK_FREQUENCY, NULL);
+//   }
+//
+//   UWU_Arena_deinit(arena);
+//   return NULL;
+// }
 
 // This RESTful server implements the following endpoints:
 //   /websocket - upgrade to Websocket, and implement websocket echo server
@@ -240,12 +251,19 @@ static void fn(struct mg_connection *c, int ev, void *ev_data) {
   }
 }
 
+void shutdown_server(int signal) {
+  fprintf(stderr, "Info: Shutting down server...\n");
+  UWU_STATE->is_shutting_off = TRUE;
+}
+
 int main(int argc, char *argv[]) {
-  struct mg_mgr mgr; // Event manager
-  int i;
+
+  struct sigaction action = {};
+  action.sa_handler = shutdown_server;
+  sigaction(SIGINT, &action, NULL);
 
   // Parse command-line flags
-  for (i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     if (strcmp(argv[i], "-url") == 0 && argv[i + 1] != NULL) {
       s_listen_on = argv[++i];
     } else if (strcmp(argv[i], "-ca") == 0 && argv[i + 1] != NULL) {
@@ -265,11 +283,19 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  mg_mgr_init(&mgr); // Initialise event manager
+  UWU_Err err = NO_ERROR;
+  UWU_ServerState state = initialize_server_state(err);
+  if (err != NO_ERROR) {
+    fprintf(stderr, "Fatal: Failed to initialize server state! Error: %zu\n",
+            *err);
+    return 1;
+  }
+  UWU_STATE = &state;
+
   printf("Starting WS listener on %s/websocket\n", s_listen_on);
-  mg_http_listen(&mgr, s_listen_on, fn, NULL); // Create HTTP listener
-  for (;;)
-    mg_mgr_poll(&mgr, 1000); // Infinite event loop
-  mg_mgr_free(&mgr);
+  mg_http_listen(&state.manager, s_listen_on, fn, NULL); // Create HTTP listener
+  for (; !UWU_STATE->is_shutting_off;)
+    mg_mgr_poll(&state.manager, 1000); // Infinite event loop
+  deinitialize_server_state(UWU_STATE);
   return 0;
 }
