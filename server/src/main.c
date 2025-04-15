bc9266d469cb7f31ba1b4c0fbad9d332568bed5d
ElrohirGT <elrohirgt@gmail.com>
2025-04-14 21:07:19 -0600

    feat: Added minimal example for mongoose server

--- server/src/main.c
+++ server/src/main.c
@@ -1,128 +1,80 @@
-/*
- * lws-minimal-ws-server
- *
- * Written in 2010-2019 by Andy Green <andy@warmcat.com>
- *
- * This file is made available under the Creative Commons CC0 1.0
- * Universal Public Domain Dedication.
- *
- * This demonstrates a minimal ws server that can cooperate with
- * other threads cleanly.  Two other threads are started, which fill
- * a ringbuffer with strings at 10Hz.
- *
- * The actual work and thread spawning etc are done in the protocol
- * implementation in protocol_lws_minimal.c.
- *
- * To keep it simple, it serves stuff in the subdirectory "./mount-origin" of
- * the directory it was started in.
- * You can change that by changing mount.origin.
- */
+// Copyright (c) 2020 Cesanta Software Limited
+// All rights reserved
+//
+// Example Websocket server. See https://mongoose.ws/tutorials/websocket-server/
 
-#include <libwebsockets.h>
-// #include "../deps/libwebsockets/build/include/libwebsockets.h"
-#include <signal.h>
-#include <string.h>
-#if defined(WIN32)
-#define HAVE_STRUCT_TIMESPEC
-#if defined(pid_t)
-#undef pid_t
-#endif
-#endif
-#include <pthread.h>
+#include "../deps/mongoose/mongoose.c"
 
-#define LWS_PLUGIN_STATIC
-#include "protocol_lws_minimal.c"
+static const char *s_listen_on = "ws://localhost:8000";
+static const char *s_web_root = ".";
+static const char *s_ca_path = "ca.pem";
+static const char *s_cert_path = "cert.pem";
+static const char *s_key_path = "key.pem";
 
-static struct lws_protocols protocols[] = {
-    {"http", lws_callback_http_dummy, 0, 0, 0, NULL, 0},
-    LWS_PLUGIN_PROTOCOL_MINIMAL,
-    LWS_PROTOCOL_LIST_TERM};
+// This RESTful server implements the following endpoints:
+//   /websocket - upgrade to Websocket, and implement websocket echo server
+//   /rest - respond with JSON string {"result": 123}
+//   any other URI serves static files from s_web_root
+static void fn(struct mg_connection *c, int ev, void *ev_data) {
+  if (ev == MG_EV_OPEN) {
+    // c->is_hexdumping = 1;
+  } else if (ev == MG_EV_ACCEPT && mg_url_is_ssl(s_listen_on)) {
+    struct mg_str ca = mg_file_read(&mg_fs_posix, s_ca_path);
+    struct mg_str cert = mg_file_read(&mg_fs_posix, s_cert_path);
+    struct mg_str key = mg_file_read(&mg_fs_posix, s_key_path);
+    struct mg_tls_opts opts = {.ca = ca, .cert = cert, .key = key};
+    mg_tls_init(c, &opts);
+  } else if (ev == MG_EV_HTTP_MSG) {
+    struct mg_http_message *hm = (struct mg_http_message *)ev_data;
+    if (mg_match(hm->uri, mg_str("/websocket"), NULL)) {
+      // Upgrade to websocket. From now on, a connection is a full-duplex
+      // Websocket connection, which will receive MG_EV_WS_MSG events.
+      mg_ws_upgrade(c, hm, NULL);
+    } else if (mg_match(hm->uri, mg_str("/rest"), NULL)) {
+      // Serve REST response
+      mg_http_reply(c, 200, "", "{\"result\": %d}\n", 123);
+    } else {
+      // Serve static files
+      struct mg_http_serve_opts opts = {.root_dir = s_web_root};
+      mg_http_serve_dir(c, ev_data, &opts);
+    }
+  } else if (ev == MG_EV_WS_MSG) {
+    // Got websocket frame. Received data is wm->data. Echo it back!
+    struct mg_ws_message *wm = (struct mg_ws_message *)ev_data;
+    mg_ws_send(c, wm->data.buf, wm->data.len, WEBSOCKET_OP_TEXT);
+  }
+}
 
-static int interrupted;
+int main(int argc, char *argv[]) {
+  struct mg_mgr mgr; // Event manager
+  int i;
 
-static const struct lws_http_mount mount = {
-    /* .mount_next */ NULL,         /* linked-list "next" */
-    /* .mountpoint */ "/",          /* mountpoint URL */
-    /* .origin */ "./mount-origin", /* serve from dir */
-    /* .def */ "index.html",        /* default filename */
-    /* .protocol */ NULL,
-    /* .cgienv */ NULL,
-    /* .extra_mimetypes */ NULL,
-    /* .interpret */ NULL,
-    /* .cgi_timeout */ 0,
-    /* .cache_max_age */ 0,
-    /* .auth_mask */ 0,
-    /* .cache_reusable */ 0,
-    /* .cache_revalidate */ 0,
-    /* .cache_intermediaries */ 0,
-    /* .origin_protocol */ LWSMPRO_FILE, /* files in a dir */
-    /* .mountpoint_len */ 1,             /* char count */
-    /* .basic_auth_login_file */ NULL,
-};
-
-/*
- * This demonstrates how to pass a pointer into a specific protocol handler
- * running on a specific vhost.  In this case, it's our default vhost and
- * we pass the pvo named "config" with the value a const char * "myconfig".
- *
- * This is the preferred way to pass configuration into a specific vhost +
- * protocol instance.
- */
-
-static const struct lws_protocol_vhost_options pvo_ops = {
-    NULL, NULL, "config", /* pvo name */
-    (void *)"myconfig"    /* pvo value */
-};
-
-static const struct lws_protocol_vhost_options pvo = {
-    NULL,          /* "next" pvo linked-list */
-    &pvo_ops,      /* "child" pvo linked-list */
-    "lws-minimal", /* protocol name we belong to on this vhost */
-    ""             /* ignored */
-};
-
-void sigint_handler(int sig) { interrupted = 1; }
-
-int main(int argc, const char **argv) {
-  struct lws_context_creation_info info;
-  struct lws_context *context;
-  const char *p;
-  int logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
-      /* for LLL_ verbosity above NOTICE to be built into lws,
-       * lws must have been configured and built with
-       * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
-      /* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
-      /* | LLL_EXT */ /* | LLL_CLIENT */  /* | LLL_LATENCY */
-      /* | LLL_DEBUG */;
-
-  signal(SIGINT, sigint_handler);
-
-  if ((p = lws_cmdline_option(argc, argv, "-d")))
-    logs = atoi(p);
-
-  lws_set_log_level(logs, NULL);
-  lwsl_user("LWS minimal ws server + threads | visit http://localhost:7681\n");
-
-  memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
-  info.port = 7681;
-  info.mounts = &mount;
-  info.protocols = protocols;
-  info.pvo = &pvo; /* per-vhost options */
-  info.options = LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
-
-  context = lws_create_context(&info);
-  if (!context) {
-    lwsl_err("lws init failed\n");
-    return 1;
+  // Parse command-line flags
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-url") == 0 && argv[i + 1] != NULL) {
+      s_listen_on = argv[++i];
+    } else if (strcmp(argv[i], "-ca") == 0 && argv[i + 1] != NULL) {
+      s_ca_path = argv[++i];
+    } else if (strcmp(argv[i], "-cert") == 0 && argv[i + 1] != NULL) {
+      s_cert_path = argv[++i];
+    } else if (strcmp(argv[i], "-key") == 0 && argv[i + 1] != NULL) {
+      s_key_path = argv[++i];
+    } else {
+      printf("Usage: %s OPTIONS\n"
+             "  -ca PATH  - Path to the CA file, default: '%s'\n"
+             "  -cert PATH  - Path to the CERT file, default: '%s'\n"
+             "  -key PATH  - Path to the KEY file, default: '%s'\n"
+             "  -url URL  - Listen on URL, default: '%s'\n",
+             argv[0], s_ca_path, s_cert_path, s_key_path, s_listen_on);
+      return 1;
+    }
   }
 
-  /* start the threads that create content */
-
-  while (!interrupted)
-    if (lws_service(context, 0))
-      interrupted = 1;
-
-  lws_context_destroy(context);
-
+  mg_mgr_init(&mgr); // Initialise event manager
+  printf("Starting WS listener on %s/websocket\n", s_listen_on);
+  mg_http_listen(&mgr, s_listen_on, fn, NULL); // Create HTTP listener
+  for (;;)
+    mg_mgr_poll(&mgr, 1000); // Infinite event loop
+  mg_mgr_free(&mgr);
   return 0;
 }
