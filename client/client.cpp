c9d457e76a0b5732384fa92c8ba39cafa34546d5
DanielRasho <ray22933@uvg.edu.gt>
2025-04-16 21:49:11 -0600

    feat: add worker class for multithread support on client message handling

--- client/client.cpp
+++ client/client.cpp
@@ -39,19 +39,19 @@ static const size_t MAX_CHARACTERS_INPUT = 254;
 // ***********************************************
 
 struct UWU_ClientState {
-  UWU_User UWU_CurrentUser;
+  UWU_User CurrentUser;
 
   // Stores the users the client can messages to.
-  UWU_UserList UWU_ActiveUsers;
+  UWU_UserList ActiveUsers;
   // A pointer to the current selected chat, set to NULL if no client is
   // selected.
-  UWU_ChatHistory *UWU_currentChat;
+  UWU_ChatHistory *currentChat;
   // Group chat user
-  UWU_User UWU_GroupChat;
+  UWU_User GroupChat;
   // Saves all the chat histories.
   // Key: The name of the user this client chat chat to.
   // Value: An UWU_History item.
-  struct hashmap_s chats;
+  struct hashmap_s Chats;
 };
 
 UWU_ClientState *UWU_STATE = NULL;
@@ -70,8 +70,31 @@ void deinit_ClientState(UWU_Err *err) {}
 // CONTROLLER
 // *******************************************
 
-// Worker function that handles a message
-static void handle_msg(struct mg_connection *c, int ev, void *ev_data);
+class Worker : public QObject {
+  Q_OBJECT
+
+public:
+  Worker(QString message, QObject *parent = nullptr)
+      : QObject(parent), message(message) {}
+
+public slots:
+  void process() {
+    qDebug() << "Processing message:" << message;
+
+    // Simulate a state change (you can modify your state here)
+    emit stateChanged("State updated after processing");
+
+    // Worker is done processing
+    emit finished();
+  }
+
+signals:
+  void stateChanged(QString newState); // Signal for state change
+  void finished();                     // Signal when worker is done processing
+
+private:
+  QString message;
+};
 
 class Controller : public QObject {
   Q_OBJECT
@@ -80,89 +103,158 @@ class Controller : public QObject {
 
 public slots:
 
-  // Callback function called by moongose on event
-  static void ws_listener(struct mg_connection *c, int ev, void *ev_data) {}
+  // Holds all logic related to receiving messages from the server.
+  // Print websocket response and signal that we're done
+  static void ws_listener(struct mg_connection *c, int ev, void *ev_data) {
+    if (ev == MG_EV_OPEN) {
+      c->is_hexdumping = 1;
+    } else if (ev == MG_EV_CONNECT && mg_url_is_ssl(s_url)) {
+      // struct mg_str ca = mg_file_read(&mg_fs_posix, s_ca_path);
+      struct mg_tls_opts opts = {.name = mg_url_host(s_url)};
+      mg_tls_init(c, &opts);
+    } else if (ev == MG_EV_ERROR) {
+      // On error, log error message
+      MG_ERROR(("%p %s", c->fd, (char *)ev_data));
+    } else if (ev == MG_EV_WS_OPEN) {
+      // When websocket handshake is successful, send message
+      printf("SENDING MESSAGE\n");
+      mg_ws_send(c, "hello", 5, WEBSOCKET_OP_TEXT);
+    } else if (ev == MG_EV_WS_MSG) {
+      const char *msg = (const char *)ev_data;
+      qDebug() << "Received WebSocket message:" << msg;
+
+      struct mg_ws_message *wm = (struct mg_ws_message *)ev_data;
+      printf("GOT ECHO REPLY: [%.*s]\n", (int)wm->data.len, wm->data.buf);
+
+      // Create a worker to process the message
+      Worker *worker = new Worker(msg);
+
+      // Create a new thread for this worker
+      QThread *workerThread = new QThread();
+
+      // Move the worker to the new thread
+      worker->moveToThread(workerThread);
+
+      // Connect signals and slots
+      QObject::connect(workerThread, &QThread::started, worker,
+                       &Worker::process);
+      QObject::connect(worker, &Worker::finished, workerThread, &QThread::quit);
+      QObject::connect(worker, &Worker::finished, worker, &Worker::deleteLater);
+      QObject::connect(workerThread, &QThread::finished, workerThread,
+                       &QThread::deleteLater);
+
+      // Connect state change signal to update state (or UI)
+      QObject::connect(worker, &Worker::stateChanged,
+                       [](const QString &newState) {
+                         qDebug() << "State changed:" << newState;
+                         // Here, you can handle the state change, such as
+                         // updating UI
+                       });
+
+      // Start the worker thread
+      workerThread->start();
+    }
+  }
 
   // Entrypoint to start the controller client
   void run() {
     qDebug() << "Worker is running in thread:" << QThread::currentThread();
 
-    mg_mgr_init(&mgr);       // Initialise event manager
-    mg_log_set(MG_LL_DEBUG); // Set log level
+    mg_mgr_init(&mgr); // Initialise event manager
     ws_conn =
         mg_ws_connect(&mgr, s_url, ws_listener, &done, NULL); // Create client
 
-    // initializing client
+    // WS Event loop
     QTimer *pollTimer = new QTimer(this);
-    connect(pollTimer, &QTimer::timeout, this, [=]() {
-      mg_mgr_poll(&mgr, 100); // Does same work, but async-friendly
-    });
+    connect(pollTimer, &QTimer::timeout, this,
+            [=]() { mg_mgr_poll(&mgr, 100); });
     pollTimer->start(50);
   }
   // Cleaning function to call when window is closed
   void stop() {
+    printf("\nCleaning resources...\n");
     mg_mgr_free(&mgr);
-    printf("Cleaning resources...");
   }
 
 signals:
   void finished();
 };
 
-static void ws_listener(struct mg_connection *c, int ev, void *ev_data) {
+// Fetch list of users
+void list_users_handler() {
+  char data[1] = {1};
+  mg_ws_send(ws_conn, data, 1, WEBSOCKET_OP_BINARY);
+  printf("LIST USERS!");
+}
 
-};
+// NOTE: MIGHT NOT BE USEFUL FOR OUR PURPOSES
+void get_user_handler() {}
 
-// Holds all logic related to receiving messages from the server.
-// Print websocket response and signal that we're done
-static void handle_msg(struct mg_connection *c, int ev, void *ev_data) {
-  if (ev == MG_EV_OPEN) {
-    c->is_hexdumping = 1;
-  } else if (ev == MG_EV_CONNECT && mg_url_is_ssl(s_url)) {
-    // struct mg_str ca = mg_file_read(&mg_fs_posix, s_ca_path);
-    struct mg_tls_opts opts = {.name = mg_url_host(s_url)};
-    mg_tls_init(c, &opts);
-  } else if (ev == MG_EV_ERROR) {
-    // On error, log error message
-    MG_ERROR(("%p %s", c->fd, (char *)ev_data));
-  } else if (ev == MG_EV_WS_OPEN) {
-    // When websocket handshake is successful, send message
-    mg_ws_send(c, "hello", 5, WEBSOCKET_OP_TEXT);
-  } else if (ev == MG_EV_WS_MSG) {
-    // When we get echo response, print it
-    struct mg_ws_message *wm = (struct mg_ws_message *)ev_data;
-    printf("GOT ECHO REPLY: [%.*s]\n", (int)wm->data.len, wm->data.buf);
-
-    switch (wm->data.buf[0]) {
-    case ERROR:
-      /* code */
-      break;
-    case LISTED_USERS:
-      /* code */
-      break;
-    case GOT_USER:
-      /* code */
-      break;
-    case REGISTERED_USER:
-      /* code */
-      break;
-    case CHANGED_STATUS:
-      /* code */
-      break;
-    case GOT_MESSAGE:
-      break;
-    case GOT_MESSAGES:
-      break;
-    default:
-      // TODO: Show toast if no message is recognized
-      fprintf(stderr, "Error: Unrecognized message from server!\n");
-      break;
-    }
+// Change status of the current user
+void tooggle_status_handler() {
+  UWU_User *currentUser = &UWU_STATE->CurrentUser;
+  size_t username_length = currentUser->username.length;
+  size_t length = 3 + username_length;
+  char *data = (char *)malloc(length);
+  if (data == NULL) {
+    UWU_PANIC("Could not allocate memory to set STATUS BUSY");
   }
 
-  if (ev == MG_EV_ERROR || ev == MG_EV_CLOSE || ev == MG_EV_WS_MSG) {
-    *(bool *)c->fn_data = true; // Signal that we're done
+  data[0] = CHANGE_STATUS;
+  data[1] = username_length;
+
+  for (size_t i = 0; i < username_length; i++) {
+    data[2 + i] = UWU_String_charAt(&currentUser->username, i);
   }
+
+  if (currentUser->status == ACTIVE || currentUser->status == INACTIVE) {
+    data[length - 1] = BUSY;
+  } else {
+    data[length - 1] = ACTIVE;
+  }
+
+  mg_ws_send(ws_conn, data, length, WEBSOCKET_OP_BINARY);
+  free(data);
+}
+
+// Fetch messages for the current chat history selected
+void get_messages_handler() {}
+
+// Sends a chat message with given history
+void send_message_handler(UWU_String *text) {
+  // TODO: handle when no chat is selected
+  UWU_ChatHistory *currentChat = UWU_STATE->currentChat;
+  if (currentChat == NULL) {
+    return;
+  }
+  size_t username_length = currentChat->channel_name.length;
+  size_t message_length = text->length;
+  size_t length = 4 + username_length + message_length;
+  printf("Total length: %d\n", length);
+  printf("username lenght: %d\n", username_length);
+  printf("message lenght: %d\n", message_length);
+  char *data = (char *)malloc(length);
+
+  if (data == NULL) {
+    UWU_PANIC("Unable to allocate memory for sending message");
+  }
+
+  data[0] = SEND_MESSAGE;
+  data[1] = username_length;
+  for (size_t i = 0; i < username_length; i++) {
+    data[2 + i] = UWU_String_charAt(&currentChat->channel_name, i);
+  }
+  // a | a | a a a | a | a
+  // 0 | 1 | 2 3 4 | 5 | 6
+  // 1   2   3 4 5   5 | 7
+  data[2 + username_length] = message_length;
+  for (size_t i = 0; i < message_length; i++) {
+    data[3 + username_length + i] = UWU_String_charAt(text, i);
+  }
+
+  mg_ws_send(ws_conn, data, length, WEBSOCKET_OP_BINARY);
+
+  free(data);
 }
 
 // *******************************************
