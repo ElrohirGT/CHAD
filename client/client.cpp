ff7bc5765636c23235d41ab5ddd328a9448bcd95
DanielRasho <ray22933@uvg.edu.gt>
2025-04-16 19:00:09 -0600

    feat: add controller class to run client business logic

--- client/client.cpp
+++ client/client.cpp
@@ -1,4 +1,6 @@
 #include "../lib/lib.c"
+#include "deps/hashmap/hashmap.h"
+#include "mongoose.h"
 #include <QAbstractItemView>
 #include <QApplication>
 #include <QCloseEvent>
@@ -17,6 +19,8 @@
 #include <QStringListModel>
 #include <QStyledItemDelegate>
 #include <QTextEdit>
+#include <QThread>
+#include <QTimer>
 #include <QVBoxLayout>
 #include <QVariant>
 #include <QWidget>
@@ -24,21 +28,146 @@
 #include <cstring>
 #include <vector>
 
+// ***********************************************
+// CONSTANTS
+// ***********************************************
+static const size_t MAX_MESSAGES_PER_CHAT = 100;
+static const size_t MAX_CHARACTERS_INPUT = 254;
+
+// ***********************************************
+// MODEL
+// ***********************************************
+
+struct UWU_ClientState {
+  UWU_User UWU_CurrentUser;
+
+  // Stores the users the client can messages to.
+  UWU_UserList UWU_ActiveUsers;
+  // A pointer to the current selected chat, set to NULL if no client is
+  // selected.
+  UWU_ChatHistory *UWU_currentChat;
+  // Group chat user
+  UWU_User UWU_GroupChat;
+  // Saves all the chat histories.
+  // Key: The name of the user this client chat chat to.
+  // Value: An UWU_History item.
+  struct hashmap_s chats;
+};
+
+UWU_ClientState *UWU_STATE = NULL;
+
+// Connection to the websocket server
+static mg_connection *ws_conn;
+
+static const char *s_url = "ws://ws.vi-server.org/mirror/";
+static const char *s_ca_path = "ca.pem";
+
+void init_ClientState(UWU_Err *err) {}
+
+void deinit_ClientState(UWU_Err *err) {}
+
 // *******************************************
 // CONTROLLER
 // *******************************************
-class Worker : public QObject {
+
+// Worker function that handles a message
+static void handle_msg(struct mg_connection *c, int ev, void *ev_data);
+
+class Controller : public QObject {
   Q_OBJECT
+  struct mg_mgr mgr;
+  bool done = false;
 
 public slots:
-  void doWork() {
+
+  // Callback function called by moongose on event
+  static void ws_listener(struct mg_connection *c, int ev, void *ev_data) {}
+
+  // Entrypoint to start the controller client
+  void run() {
     qDebug() << "Worker is running in thread:" << QThread::currentThread();
-    // Placeholder work
+
+    mg_mgr_init(&mgr);       // Initialise event manager
+    mg_log_set(MG_LL_DEBUG); // Set log level
+    ws_conn =
+        mg_ws_connect(&mgr, s_url, ws_listener, &done, NULL); // Create client
+
+    // initializing client
+    QTimer *pollTimer = new QTimer(this);
+    connect(pollTimer, &QTimer::timeout, this, [=]() {
+      mg_mgr_poll(&mgr, 100); // Does same work, but async-friendly
+    });
+    pollTimer->start(50);
+  }
+  // Cleaning function to call when window is closed
+  void stop() {
+    mg_mgr_free(&mgr);
+    printf("Cleaning resources...");
   }
 
 signals:
   void finished();
 };
+
+static void ws_listener(struct mg_connection *c, int ev, void *ev_data) {
+
+};
+
+// Holds all logic related to receiving messages from the server.
+// Print websocket response and signal that we're done
+static void handle_msg(struct mg_connection *c, int ev, void *ev_data) {
+  if (ev == MG_EV_OPEN) {
+    c->is_hexdumping = 1;
+  } else if (ev == MG_EV_CONNECT && mg_url_is_ssl(s_url)) {
+    // struct mg_str ca = mg_file_read(&mg_fs_posix, s_ca_path);
+    struct mg_tls_opts opts = {.name = mg_url_host(s_url)};
+    mg_tls_init(c, &opts);
+  } else if (ev == MG_EV_ERROR) {
+    // On error, log error message
+    MG_ERROR(("%p %s", c->fd, (char *)ev_data));
+  } else if (ev == MG_EV_WS_OPEN) {
+    // When websocket handshake is successful, send message
+    mg_ws_send(c, "hello", 5, WEBSOCKET_OP_TEXT);
+  } else if (ev == MG_EV_WS_MSG) {
+    // When we get echo response, print it
+    struct mg_ws_message *wm = (struct mg_ws_message *)ev_data;
+    printf("GOT ECHO REPLY: [%.*s]\n", (int)wm->data.len, wm->data.buf);
+
+    switch (wm->data.buf[0]) {
+    case ERROR:
+      /* code */
+      break;
+    case LISTED_USERS:
+      /* code */
+      break;
+    case GOT_USER:
+      /* code */
+      break;
+    case REGISTERED_USER:
+      /* code */
+      break;
+    case CHANGED_STATUS:
+      /* code */
+      break;
+    case GOT_MESSAGE:
+      break;
+    case GOT_MESSAGES:
+      break;
+    default:
+      // TODO: Show toast if no message is recognized
+      fprintf(stderr, "Error: Unrecognized message from server!\n");
+      break;
+    }
+  }
+
+  if (ev == MG_EV_ERROR || ev == MG_EV_CLOSE || ev == MG_EV_WS_MSG) {
+    *(bool *)c->fn_data = true; // Signal that we're done
+  }
+}
+
+// *******************************************
+// VIEW
+// *******************************************
 
 // class for creating the window project
 class MainWindow : public QMainWindow {
@@ -236,6 +365,30 @@ public:
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
+
+  // **********************
+  // CONTROLLER AND WS CLIENT CONNECTION INITALIZAITON
+  // **********************
+
+  QThread *thread = new QThread;
+  Controller *controller = new Controller;
+  controller->moveToThread(thread);
+
+  QObject::connect(thread, &QThread::started, controller, &Controller::run);
+  QObject::connect(controller, &Controller::finished, thread, &QThread::quit);
+  QObject::connect(controller, &Controller::finished, controller,
+                   &Controller::deleteLater);
+  QObject::connect(thread, &QThread::finished, controller,
+                   &QThread::deleteLater);
+  QObject::connect(&app, &QCoreApplication::aboutToQuit, controller,
+                   &Controller::stop);
+
+  // Start the thread
+  thread->start();
+
+  // **********************
+  // LAYOUT INITIALIZATION
+  // **********************
 
   std::vector<UserData> users = {
       {"Jose", "1.2.3"}, {"Maria", "4.5.6"}, {"Pedro", "7.8.9"},
